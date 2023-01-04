9132c849bf069a14f98079ab5ebeba937efc54b6
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2023-01-04 07:43:19 +0100

    Update sly.js

--- sly.js
+++ sly.js
@@ -1,26 +1,16 @@
-var i=0;
-  var images=[];
-  var time =3000;
-
-  //image list
-  images[0]='b1.jpg';
-  images[1]='b2.jpg';
-  images[2]='b3.jpg';
-  images[3]='b4.jpg';
-
-
-  //change images
-  function changeimg(argument) {
-    // body...
-    document.slide.src=images[i];
-    
-    if (i< images.length - 1) {
-      i++;
-    }
-    else{
-      i=0;
-    }
-    setTimeout("changeimg()",time);
-  }
-  window.onload = changeimg;
-
+const swiper = new Swiper('.swiper', 
+ {  
+ autoplay: {
+     delay:3000,
+     disableOnInteraction:false,
+ },
+             
+         
+    loop: true, pagination: { el: '.swiper-pagination',
+         clickable:true, 
+         
+         }, 
+         // Navigation arrows 
+         navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', },
+         });
+          
